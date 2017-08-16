5214d9d2ce3a7cdfe57ead2181bc27861fc410df
lordlycat <lordly_cat@163.com>
2017-08-16 13:23:34 +0800

    updata

--- 05/app.js
+++ 05/app.js
@@ -4,18 +4,18 @@ const path = require('path');
 const mysql = require('mysql');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8806;
+const port = 3366;
 
 // var Client = mysql.Client,
 //     client = new Client();
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '88068806',
-  database : 'upload'
+    host: 'localhost',
+    user: 'root',
+    password: '88068806',
+    database: 'upload'
 });
- 
+
 //连接数据库
 connection.connect();
 var sql = 'INSERT INTO text (img) VALUES (data)';
@@ -24,23 +24,41 @@ var sql = 'INSERT INTO text (img) VALUES (data)';
 app.use(express.static(path.join(__dirname, 'public/')));
 
 //解析数据
-app.use(bodyParser.text({type: 'text/plain'}));
-var data = '';
-var index = 0;
-//var test = '9900';
+app.use(bodyParser.json());
+/*app.use(bodyParser.text({
+  type: 'text/plain'
+}));*/
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+//var data = '';
+//var index = 0;
+
+
 //处理请求
 app.post('/', function(req, res) {
     //拼接数据
-    data += req.body;
+    var data = req.body;
+    var name = data[0];
+    var id;
 
-    //传输完毕，存入数据库
-    if (req.body === '') {
-        connection.query("INSERT INTO text (img) VALUES (data)");
-        //console.log(typeof(data));
+    //存入数据库
+    if (parseInt(data[2]) === 0) {
+        connection.query("INSERT INTO files SET name=?, data=?", [data[0], data[1]]);
+
+    } else {
+        connection.query("SELECT max(id) FROM files", function(err, result, fields) {
+            id = result[0]['max(id)'];
+            //console.log(id);
+            connection.query("UPDATE files SET data = CONCAT(data, ?) WHERE id=?", [data[1], id]);
+
+        });
+
     }
-    
+    //console.log(req.body[2]);
     res.send('1');
 });
+
 
 app.listen(port, () => {
     console.log('App is listening at ' + port);
--- 05/public/js/index.js
+++ 05/public/js/index.js
@@ -4,26 +4,32 @@ var size = 0;
 var index = 0;
 var sign = false;
 var num = 0;
+var name = '';
 var done = document.querySelector('.done');
+var compeleted = document.querySelector('.compeleted');
+var img = document.querySelector('img');
 
 function ProcessFile(e) {
-    var file = document.getElementById('file').files[0];
+    done.innerHTML = '0%';
+    compeleted.style.width = '0px';
+    localStorage.setItem('num', 0);
+    var file = document.querySelector('#file').files[0];
     if (file) {
-
+        name = file.name;
         var reader = new FileReader();
 
         reader.onloadend = function(event) {
             num = 0;
             txt = event.target.result;
-            size = parseInt(txt.length / 1024);
-
+            size = parseInt(txt.length / (1024 * 2));
+            img.src = txt;
             console.log(txt.length / 1024);
-            document.getElementById("result").innerHTML = txt;
+            //document.getElementById("result").innerHTML = txt;
 
             //截取字符串，把文件分段
             for (var i = 0; i < size + 1; i++) {
-                dataArr[i] = txt.substr(index, 1024);
-                index += 1024;
+                dataArr[i] = txt.substr(index, 1024 * 2);
+                index += 1024 * 2;
             }
             sign = true;
         };
@@ -37,40 +43,66 @@ function ProcessFile(e) {
 }
 
 function contentLoaded() {
-    document.getElementById('file').addEventListener('change',
+    document.querySelector('#file').addEventListener('change',
         ProcessFile, false);
 }
 window.addEventListener("DOMContentLoaded", contentLoaded, false);
 
 
 //上传
-var btn = document.querySelector('button');
+var uploadBtn = document.querySelector('.upload');
+var pauseBtn = document.querySelector('.pause');
+var continueBtn = document.querySelector('.continue');
 
-btn.addEventListener('click', function() {
+uploadBtn.addEventListener('click', function() {
     if (sign) {
-        function send() {
-            if (num <= size + 1) {
-                ajax({
-                    url: 'http://127.0.0.1:8806/',
-                    method: 'POST',
-                    contentType: 'text/plain',
-                    data: dataArr[size + 1],
-                    success: function(data) {
-                        num += 1;
-                        console.log(num);
-                        done.innerHTML = 100*(num - 2)/size + '%';
-                        send();
-                    },
-                    error: function(data) {
-                        console.log(data);
-                    }
-                });
-            }
-        }
         send();
+        sign = false;
     } else {
         alert('文件尚未加载完毕，请稍后');
 
     }
 }, false);
 
+var continueLoad = true;
+//发送数据
+function send() {
+    num = parseInt(localStorage.getItem('num'));
+    if (num <= size + 1) {
+        ajax({
+            url: 'http://127.0.0.1:8806/',
+            method: 'POST',
+            //contentType: 'text/plain',
+            data: {
+                0: name,
+                1: dataArr[num],
+                2: num
+            },
+            success: function(data) {
+                num += 1;
+                //记录下次要上传的地方
+                localStorage.setItem('num', num);
+                console.log(num);
+                done.innerHTML = (100 * (num - 1) / (size + 1)).toFixed(2) + '%';
+                compeleted.style.width = ((num - 1) / (size + 1)) * 500 + 'px';
+                if (continueLoad) {
+                    send();
+                }
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    }
+}
+
+//暂停上传
+pauseBtn.addEventListener('click', function() {
+    continueLoad = false;
+}, false);
+
+//继续上传
+continueBtn.addEventListener('click', function() {
+    continueLoad = true;
+    send();
+}, false);
